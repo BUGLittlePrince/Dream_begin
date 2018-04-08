c9a3a5a8b38b10dbe388d0b3cd72e5e2235a0b82
Gaoyang <goyaya@yeah.net>
2018-04-08 15:23:44 +0800

    Initial Commit

---
+++ Dream_Begin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dream_Begin
+//
+//  Created by hanhong on 2018/4/8.
+//  Copyright © 2018年 hanhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dream_Begin/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dream_Begin
+//
+//  Created by hanhong on 2018/4/8.
+//  Copyright © 2018年 hanhong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
